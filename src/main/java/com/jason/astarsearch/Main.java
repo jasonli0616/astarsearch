f09a3cb2098c8d9e1c4b61dc4df51c6a87932e46
Jason Li <jasonli.0616@outlook.com>
2023-06-19 11:04:04 -0400

    Modify Main to allow page changing

--- src/main/java/com/jason/astarsearch/Main.java
+++ src/main/java/com/jason/astarsearch/Main.java
@@ -2,19 +2,34 @@ package com.jason.astarsearch;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Scene scene;
+    private static Stage stage;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("homepage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+    public void start(Stage newStage) throws IOException {
+        stage = newStage;
+
+        scene = new Scene(loadFXML("homepage"), 600, 400);
         stage.setTitle("A* Search");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void setRoot(String root) throws IOException {
+        scene.setRoot(loadFXML(root));
+    }
+
+    private static Parent loadFXML(String path) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(path + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
